a415683f44ebed35962c3eb40a95b2c116906383
jasonoverby <disguisekit@gmail.com>
2016-09-07 06:23:53 -0700

    added initial play

--- game.rb
+++ game.rb
@@ -1,14 +1,14 @@
 $LOAD_PATH << '.'
 require "game_module"
+include WriterStuff
 
 class Test
-  include Greet
 
   def initialize
     # variable must be included in initialize or def for
     # object to have access to it
     # & must be an instance variable
-    @punctuation = %w(. ! ?)
+    # @punctuation = %w(. ! ?)
   end
 
   def greeting(name)
@@ -16,7 +16,7 @@ class Test
     hello = "HELLLLLLOOOOOOO"
 
 
-    puts "#{test_greeting}, #{name}#{@punctuation[rand(0..@punctuation.length - 1)]}"
+    puts " #{name}#{punctuation[rand(0..punctuation.length - 1)]}"
     # TRY WITH %s
     # puts format("%s, %s%s", test_greeting, name, @punctuation[rand(0..@punctuation.length - 1)])
     # puts sprintf("%s, %s%s", test_greeting, name, @punctuation[rand(0..@punctuation.length - 1)])
@@ -25,13 +25,69 @@ class Test
   end
 end
 
+class Play
+  def initialize(map)
+    $map = map
+  end
+
+  def start
+    get_name
+    puts "OK, #{name}..."
+
+    # get_favorite_tool
+    # puts "You like to use a #{favorite_tool}?"
+    # filler
+    # puts "I guess that's cool.  It takes all kinds."
+
+    $map.starting_room
+  end
+
+  def writer
+    [name, favorite_book, favorite_tool]
+  end
+
+end
+
 class Room
-  def hello
-    puts "Hello, Virld!"
+end
+
+class BookStore < Room
+
+  def enter
+    puts "You are in a bookstore."
+    puts "What do you do?"
+    puts "1. Look at the shelves"
+    puts "2. Get a coffee"
+    puts "3. Go to the bathroom"
+    print "> "
+    choice = $stdin.gets.chomp.to_i
+
+    if choice == 1
+      puts "You languidly peruse the particle board stacks."
+      puts "Some books on an end cap catch your eye..."
+      get_favorite_book
+      filler
+      puts "You like #{favorite_book}?"
+      filler
+      puts "What do you do now?"
+      action = $stdin.gets.chomp.downcase
+      if action.include?("read")
+        puts "Your can read?!?"
+        $map.next_room("cafe")
+      else
+        puts "I don't understand!"
+      end
+    else
+      puts "You are dumb!"
+    end
   end
 end
 
 class Cafe < Room
+  def enter
+    puts "You have entered a pirate-themed café"
+    puts "Ahoy, Matey!"
+  end
 end
 
 class Study < Room
@@ -40,8 +96,25 @@ end
 class Library < Room
 end
 
-t = Test.new
-t.greeting("Jason")
+class Map
+  def initialize(first_room)
+    @first_room = first_room
+  end
 
-r = Cafe.new
-r.hello
+  def rooms
+    {"bookstore" => BookStore.new.enter,
+     "cafe" => Cafe.new.enter}
+  end
+
+  def starting_room
+    rooms.find(@first_room)
+  end
+
+  def next_room(room)
+    rooms.find(room)
+  end
+end
+
+m = Map.new("bookstore")
+p = Play.new(m)
+p.start
--- game_module.rb
+++ game_module.rb
@@ -1,5 +1,73 @@
-module Greet
-  def test_greeting
-    "Hello"
+module WriterStuff
+  def punctuation
+    %w(. ! ? ¡ ¿)
+  end
+
+  def filler
+    filler_words = %w(Hmmmm Well Er Um Huh Ok)
+    puts "#{filler_words[rand(0..filler_words.length - 1)]}..."
+  end
+
+  def get_name
+    puts "Hello, there!"
+    puts "What is your name?"
+    print "> "
+    @name = $stdin.gets.chomp
+  end
+
+  def name
+    @name
+  end
+
+  def book_titles
+    ["On the Road", "Demian", "Women", "Dune", "Anna Karenina", "Love in the Time of Cholera"]
+  end
+
+  def book_authors
+    ["Jack Kerouac", "Hermann Hesse", "Charles Bukowski", "Frank Herbert", "Leo Tolstoy", "Garbiel Garcia Marquez"]
+  end
+
+  def books
+    Hash[book_authors.zip book_titles]
+  end
+
+  def get_favorite_book
+    puts "Whick of these tomes do you like best?"
+    book_array = []
+    books.each do |author, title|
+      book_array << "'#{title}' by #{author}"
+    end
+    book_array.each_with_index do |book,index|
+      puts "#{index + 1}. #{book}"
+    end
+    print "> "
+    @favorite_book = book_array[$stdin.gets.chomp.to_i - 1]
+  end
+
+  def favorite_book
+    @favorite_book
+  end
+
+  def tools
+    %w(computer pen pencil notebook)
+  end
+
+  def list_tools
+    tools.each_with_index do |tool,index|
+      # print " "
+      puts "#{index + 1}. #{tool}"
+    end
+  end
+
+  def get_favorite_tool
+    puts "Ok, so you call yourself a writer, right?"
+    puts "Which of these is your favorite tool?"
+    list_tools
+    print "> "
+    @favorite_tool = tools[$stdin.gets.chomp.to_i - 1]
+  end
+
+  def favorite_tool
+    @favorite_tool
   end
 end
